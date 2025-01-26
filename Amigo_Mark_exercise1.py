2fe4a0367ead9d51370fb86c8247fa0b30afe78b
MarkAmigo <markamigo0313@gmail.com>
2025-01-26 10:22:05 -0500

    Update Amigo_Mark_exercise1.py

--- Amigo_Mark_exercise1.py
+++ Amigo_Mark_exercise1.py
@@ -5,6 +5,9 @@
 # Basic printing of information
 
 name="<Mark Amigo>"
+print(name)
 print("C0P2002-0M2")
 print("Spring 2025")
 print("This is saved in a Python file!")
+print()
+print("I'm understanding how to use GitHub to keep track of changes!")
