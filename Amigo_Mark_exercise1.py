652c9e771bfc809956461059354846e44da9d1b8
MarkAmigo <markamigo0313@gmail.com>
2025-01-26 10:15:52 -0500

    Add files via upload

---
+++ Amigo_Mark_exercise1.py
@@ -0,0 +1,10 @@
+# Mark Amigo (MarkAmigo)
+# C0P2002-0M2
+# January 26, 2025
+# Exercise 1
+# Basic printing of information
+
+name="<Mark Amigo>"
+print("C0P2002-0M2")
+print("Spring 2025")
+print("This is saved in a Python file!")
